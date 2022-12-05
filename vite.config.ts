736f4063f322bc8f0655c99496996123785c9c61
Gustavo Muñoz <timbergus@gmail.com>
2022-12-05 21:08:20 +0100

    Add pages.

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  build: {
+    outDir: 'docs',
+    chunkSizeWarningLimit: 1000,
+  },
 })
